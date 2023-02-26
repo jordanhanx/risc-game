8eb0a556223a6208c139f1d7bd682163f5ec8499
Xu (Jordan) Han <xu.han@duke.edu>
2023-02-26 16:07:22 -0500

    change teamX to team7

---
+++ gradle-skeleton/server/src/test/java/edu/duke/ece651/team7/server/AppTest.java
@@ -0,0 +1,15 @@
+package edu.duke.ece651.team7.server;
+
+import org.junit.jupiter.api.Test;
+
+import edu.duke.ece651.team7.server.App;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+  @Test
+  void test_GetMessage() {
+    App a = new App();
+    assertEquals("Hello from the server for team7", a.getMessage());
+  }
+}
--- gradle-skeleton/shared/src/test/java/edu/duke/ece651/team7/shared/MyNameTest.java
+++ gradle-skeleton/shared/src/test/java/edu/duke/ece651/team7/shared/MyNameTest.java
@@ -1,13 +1,15 @@
-package edu.duke.ece651.teamX.shared;
+package edu.duke.ece651.team7.shared;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.duke.ece651.team7.shared.MyName;
+
 public class MyNameTest {
   @Test
   public void test_getName() {
-    assertEquals("teamX", MyName.getName());
+    assertEquals("team7", MyName.getName());
   }
 
 }
