5edec299a6d94651290b4f926f618f84293c7111
Zhecheng Dong <zhecheng.dong@duke.edu>
2023-03-03 00:46:11 -0500

    Merge branch 'master' of https://gitlab.oit.duke.edu/xh123/ece651-sp23-team7-risk into MinimalMapClassTask

---
+++ shared/src/test/java/edu/duke/ece651/team7/shared/MapTest.java
@@ -0,0 +1,21 @@
+package edu.duke.ece651.team7.shared;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashSet;
+
+
+import org.junit.jupiter.api.Test;
+
+public class MapTest {
+  @Test
+  public void test_constructor() {
+    HashSet<Territory> territories = new HashSet<Territory>();
+    Territory territory = new Territory("testMap");
+    territories.add(territory);
+    Map m = new Map(territories);
+    assertEquals(territories,m.getTerritoriesSet());
+  }
+
+}
+
