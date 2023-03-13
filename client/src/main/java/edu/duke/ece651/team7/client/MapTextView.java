0c956c4a9cec4bf53d5d2d0f66dc2e55b00211e6
Xu (Jordan) Han <xh123@duke.edu>
2023-03-13 02:58:18 -0400

    remove getMessage

--- client/src/main/java/edu/duke/ece651/team7/client/MapTextView.java
+++ client/src/main/java/edu/duke/ece651/team7/client/MapTextView.java
@@ -1,4 +1,4 @@
-package edu.duke.ece651.team7;
+package edu.duke.ece651.team7.client;
 
 import java.io.PrintStream;
 import java.util.HashSet;
@@ -6,27 +6,29 @@ import java.util.HashSet;
 import edu.duke.ece651.team7.shared.*;
 
 public class MapTextView {
+
     public void display(PrintStream out, GameMap map) {
-        HashSet<Player> playerSet = HashSet<>();
-        for (Territory t : map.getTerritories()) {
-            playerSet.add(t.getOwner());
-        }
-        StringBuilder text = new StringBuilder("");
-        for (Player p : playerSet) {
-            String playerDecl = p.getName() + "player:";
-            text.append(playerDecl + "\n");
-            text.append("_".repeat(playerDecl.length()) + "\n");
-            for (Territory t : p.getTerritories()) {
-                text.append(String.format("%3s",t.getUnits()) + " units in " + t.getName() + " (next to: ");
-                String sep = "";
-                for (Territory n : t.getNeighbors()) {
-                    text.append(sep);
-                    text.append(n.getName());
-                    sep = ", ";
-                }
-                text.append(")\n");
-            }
-            text.append("\n");
-        }
+        HashSet<Player> playerSet = new HashSet<>();
+        // for (Territory t : map.getTerritories()) {
+        // playerSet.add(t.getOwner());
+        // }
+        // StringBuilder text = new StringBuilder("");
+        // for (Player p : playerSet) {
+        // String playerDecl = p.getName() + "player:";
+        // text.append(playerDecl + "\n");
+        // text.append("_".repeat(playerDecl.length()) + "\n");
+        // for (Territory t : p.getTerritories()) {
+        // text.append(String.format("%3s", t.getUnits()) + " units in " + t.getName() +
+        // " (next to: ");
+        // String sep = "";
+        // for (Territory n : t.getNeighbors()) {
+        // text.append(sep);
+        // text.append(n.getName());
+        // sep = ", ";
+        // }
+        // text.append(")\n");
+        // }
+        // text.append("\n");
+        // }
     }
 }
