847e3e3e756ca0bae77db792fb8703a7542b4e12
Ellen Fan <ellen.fan.rui@gmail.com>
2023-03-31 19:07:43 -0400

    shared passed test, server and client not modified

--- shared/src/main/java/edu/duke/ece651/team7/shared/ResourceProducer.java
+++ shared/src/main/java/edu/duke/ece651/team7/shared/ResourceProducer.java
@@ -1,5 +1,7 @@
 package edu.duke.ece651.team7.shared;
-public interface ResourceProducer {
-    public int produce();
+public abstract class ResourceProducer {
+    public int produce(){
+        return 1;
+    }
     
 }
