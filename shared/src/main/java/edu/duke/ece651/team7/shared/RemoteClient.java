0a08ebcadc099a3dce2b2b5f5e75f7240240fa11
Xu (Jordan) Han <51397412+hanxu23675@users.noreply.github.com>
2023-04-10 20:16:11 -0400

    update remote interfaces

--- shared/src/main/java/edu/duke/ece651/team7/shared/RemoteClient.java
+++ shared/src/main/java/edu/duke/ece651/team7/shared/RemoteClient.java
@@ -8,33 +8,8 @@ import java.rmi.RemoteException;
  * only methods defined here can be invoked on remote side.
  */
 public interface RemoteClient extends Remote {
-    /**
-     * Detect if the remote Client is alive.
-     * 
-     * @throws RemoteException if there is an error with the remote connection
-     */
-    public void ping() throws RemoteException;
 
-    /**
-     * Force the client to display the map.
-     * 
-     * @param map is the GameMap
-     * @throws RemoteException
-     */
-    public void doDisplay(GameMap map) throws RemoteException;
+    public void updateGameMap(GameMap gameMap) throws RemoteException;
 
-    /**
-     * Force the client to display the String message.
-     * 
-     * @param msg is the message
-     * @throws RemoteException if there is an error with the remote connection
-     */
-    public void doDisplay(String msg) throws RemoteException;
-
-    /**
-     * Force the close the client.
-     * 
-     * @throws RemoteException if there is an error with the remote connection
-     */
-    public void close() throws RemoteException;
+    public void showPopupWindow(String msg) throws RemoteException;
 }
